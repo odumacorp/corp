f08cc11fea0dfa98e47aba8b62462fa96ae3d48b
Odumacorp Admin <odumacorp@gmail.com>
2026-04-14 11:25:37 +0300

    created create_default_admin.py

---
+++ core/management/commands/create_default_admin.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = "Create or update default admin user"
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write(self.style.ERROR("Missing environment variables"))
+            return
+
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={"email": email}
+        )
+
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        if created:
+            self.stdout.write(self.style.SUCCESS("Superuser created"))
+        else:
+            self.stdout.write(self.style.SUCCESS("Superuser updated"))
